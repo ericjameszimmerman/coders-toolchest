4fb65ac799e3ba0892384db8a4c54ca2804cbb5a
Eric Zimmerman <ericjzim@gmail.com>
2020-02-23 00:38:16 -0600

    initial navigation attempt

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,6 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-spacer></v-spacer>
 
       <v-btn
@@ -37,21 +17,26 @@
       </v-btn>
     </v-app-bar>
 
+    <NavDrawer>
+      
+    </NavDrawer>
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import NavDrawer from './components/NavDrawer.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld,
+    NavDrawer,
   },
 
   data: () => ({
---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-navigation-drawer
+    permanent
+    expand-on-hover
+    :clipped="clipped"
+    v-model="drawer"
+    app
+  >
+    <v-list>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-content>
+          <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+          <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item link @click="navigate('/numberconverter')">
+        <v-list-item-icon>
+          <v-icon>mdi-folder</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>My Files</v-list-item-title>
+      </v-list-item>
+      <v-list-item link @click="navigate('/about')">
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Shared with me</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-star</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Starred</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'NavDrawer',
+  methods: {
+    navigate (route) {
+      this.$router.push(route);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
